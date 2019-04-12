088a77e9f0c596b69af2dda03e8c12fde86f8147
woonji913 <johnnyboy0913@gmail.com>
2019-04-12 06:25:06 +0000

    04_Upload Multiple Images

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
 class Post(models.Model):
     content = models.TextField()
-    image = models.ImageField(blank=True)
+    # image = models.ImageField(blank=True)
     
     def __ste__(self):
         return self.content
+        
+class Image(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    file = ProcessedImageField(
+                upload_to='posts/images',							
+                processors=[ResizeToFill(600, 600)],
+                )
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
-from .models import Post
-from .forms import PostForm
+from .models import Post, Image
+from .forms import PostForm, ImageForm
 
 # Create your views here.
 def list(request):  # index임
@@ -11,13 +11,27 @@ def list(request):  # index임
 
 def create(request):
     if request.method == 'POST':
-        post_form = PostForm(request.POST, request.FILES)
+        post_form = PostForm(request.POST)
         if post_form.is_valid():
-            post_form.save()
+            post = post_form.save() # 게시글 내용 처리 끝
+            
+            # 이미지 여러장 받기
+            for image in request.FILES.getlist('file'):
+                request.FILES['file'] = image
+                image_form = ImageForm(files=request.FILES)
+                if image_form.is_valid():
+                    image = image_form.save(commit=False)
+                    image.post = post
+                    image.save()
             return redirect('posts:list')
+            
     else:
         post_form = PostForm()
-    context = {'post_form':post_form,}
+        image_form = ImageForm()
+    context = {
+        'post_form':post_form,
+        'image_form': image_form,
+    }
     return render(request, 'posts/form.html', context)
 
 # def read(request, post_pk):
